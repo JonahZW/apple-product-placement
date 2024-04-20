b5bcf5e042144b0ed164488cd1cfa7bb203028f6
JonahZW <jonah@werbel.org>
2024-04-20 18:45:10 -0400

    Reorganize repo for final submission (a bit), fix broken link

--- sprints/4-dashboard-v0/app-v0.py
+++ sprints/4-dashboard-v0/app-v0.py
@@ -4,16 +4,12 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import dash_bootstrap_components as dbc
-from dash_bootstrap_templates import load_figure_template
 
 
 # initialize app
-dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css" # css for dash w bootstrap
-stylesheets = [dbc.themes.LUX, dbc_css] # load the CSS stylesheet
+stylesheets = [dbc.themes.ZEPHYR, 'https://codepen.io/chriddyp/pen/bWLwgP.css'] # load the CSS stylesheet
 app = Dash(__name__, external_stylesheets=stylesheets) # initialize the app
 server = app.server # make it work for render
-
-load_figure_template("lux")
 
 
 # read CSV with data in
@@ -27,34 +23,21 @@ df = df[df['startYear'] > 2019]
 
 # include instructions for reference
 instructions = html.Div([
-    html.H3('Instructions', className='card-title'),
+    html.H3('Instructions'),
     html.P('Interact with the selector widgets below to update the displays. Utilize these \
            displays to visualize the effect and prevalence of Apple\'s product placements in \
            popular TV shows and movies.'),
     html.P('Selecting specific titles, years, devices, or other filters make it easy to dive into \
            an in-depth exploration of where many of Apple\'s product placements occured, and to \
            potentially visualize any correlation between usage and perception of the title.'),
-    html.P('Deselect all titles, devices, and/or media types to see all of the relevant category')
-], className='card-header', style={'text-align':'justify'})
+    html.Br()
+], className='row', style={'text-align':'justify'})
 
 # now, the actual widgets/tools
 
 ## first, going to create each one
-@callback(
-        Output('title','options'),
-        Input('type','value'),
-        Input('year','value')
-)
-def title(type, year):
-    if type == None or type == []:
-        type = df['Media'].unique()
-    if year == None or year == []:
-        year = df['startYear'].unique()
-        year = [year[0], year[(len(year)-1)]]
-    return [{'label': i, 'value': i} for i in df.loc[df['Media'].isin(type) & (df['startYear'] >= year[0]) & (df['startYear'] <= year[1])]['Title'].unique()]
-
 title_dropdown = html.Div([
-    html.Label('Specific Title (select none to see all)'),
+    html.Label('Title'),
     dcc.Dropdown(id = 'title', options = df['Title'].unique(), multi = True),
     html.Br()
 ])
@@ -93,11 +76,11 @@ imgCount_slider = html.Div([
     html.Label('Number of Apple Devices per Instance'),
     dcc.RangeSlider(
         id = 'imgCount',
-        min = imgCount[0]-1, 
+        min = imgCount[0], 
         max = imgCount[len(imgCount)-1], 
         step = 1,
         value = [imgCount[0], imgCount[len(imgCount)-1]], #default values
-        marks={i: '{}'.format(i) for i in range(imgCount[0]-1, imgCount[len(imgCount)-1]+1, 10)},
+        marks={i: '{}'.format(i) for i in range(imgCount[0], imgCount[len(imgCount)-1]+1, 10)},
         allowCross=False,
     ),
     html.Br()
@@ -119,32 +102,23 @@ rating_slider = html.Div([
     html.Br()
 ])
 
-### create a mini div for the accordion of "advanced" widgets:
-accordion = html.Div([
-    dbc.Accordion([
-        dbc.AccordionItem([
-            title_dropdown,
-            imgCount_slider,
-            rating_slider
-        ], title="Advanced Selections",)
-    ], start_collapsed=True, flush=True)
-], className='card border-secondary mb-3 p-0')
-
-
 ## now, add each widget to the collection of widgets, to be added to the navBar overall
 widgets = html.Div([
-    html.H3('Selections', className='card-title'),
-    type_dropdown,
+    html.H3('Selections'),
+    title_dropdown,
     year_slider,
+    type_dropdown,
     device_dropdown,
-    accordion
-], className='card-body')
+    html.H5('Advanced Selections'),
+    imgCount_slider,
+    rating_slider
+])
 
 # and this is the final div which will be added to the layout representing the entire navbar
 navBar = html.Div([
     instructions,
     widgets
-], className='card border-primary mb-3 col-lg-3 p-0', style={'height':'100%'})
+], className='three columns', style={'height':'100%'})
 
 
 # *************************************************************************************
@@ -179,8 +153,7 @@ def pie_devices(title, year, type, device, imgCount, rating):
         resolved_df.groupby(['Device'])['Device'].count().reset_index(name='count'),
         values='count',
         names='Device',
-        title='Apple Products Placed in Titles by Device',
-        height=500,
+        title='Apple Products Placed in Titles by Device'
     )
     return fig
 
@@ -232,7 +205,6 @@ def scatter_ratings(title, year, type, device, imgCount, rating):
         },
         title='Product Placements by Title vs Average Rating'
     )
-    fig.update_traces(marker=dict(size=9, line=dict(width=0.5,color='DarkSlateGrey')), selector=dict(mode='markers'))
     return fig
 
 # creates line chart showing number of devices in titles over time
@@ -243,10 +215,9 @@ def scatter_ratings(title, year, type, device, imgCount, rating):
         Input('type','value'),
         Input('device','value'),
         Input('imgCount','value'),
-        Input('rating','value'),
-        Input('line_device_radio','value')
+        Input('rating','value')
 )
-def line_device_time(title, year, type, device, imgCount, rating,line_device_radio):
+def line_device_time(title, year, type, device, imgCount, rating):
     # set defaults if not set
     if title == None or title == []:
         title = df['Title'].unique()
@@ -260,15 +231,9 @@ def line_device_time(title, year, type, device, imgCount, rating,line_device_rad
                          & (df['averageRating'] <= rating[1])]
     same_cols = ['startYear','Device']
     agg_dict = {
-        'imgCount':line_device_radio, # radio button to swap this b/w count, mean, sum
+        'imgCount':'count', # add radio button to swap this b/w count, mean, sum
         'averageRating':'mean' 
     }
-    if line_device_radio == 'count':
-        y_axis_title = 'Titles with 1+ Device Instance in Year'
-    elif line_device_radio == 'sum':
-        y_axis_title = 'Total Devices in Year'
-    elif line_device_radio == 'mean':
-        y_axis_title = 'Avg Instances of Device per Title in Year'
     resolved_df = resolved_df.drop(columns=['tconst','Title','numVotes','Media','Season','Episode'])
     resolved_df = resolved_df.groupby(same_cols).agg(agg_dict).reset_index()
     fig = px.line(
@@ -277,15 +242,9 @@ def line_device_time(title, year, type, device, imgCount, rating,line_device_rad
         y='imgCount',
         color='Device',
         markers=True,
-        title='Number of Placements by Device over Time',
-        labels={
-            'imgCount':y_axis_title,
-            'startYear':'Year'
-        },
-        height=420
+        title='Number of Placements by Device over Time'
     )
     fig.update_xaxes(type='category')
-    fig.update_traces(marker=dict(size=9, line=dict(width=0.5,color='DarkSlateGrey')), line=dict(width=4))
     return fig
 
 ## Second, I combine all the figures/parts in correct divs
@@ -298,74 +257,46 @@ pie_devices_div = html.Div([
     dcc.Graph(
         figure = pie_devices(None, [0,0],None,None,[0,0],[0,0]), id='pie-devices'
     ),
-], className='card border-secondary mr-1', style={'float':'left', 'width':'36%', 'margin-right':'1%'})
+], className='four columns')
 
 scatter_ratings_div = html.Div([
     dcc.Graph(
         figure = scatter_ratings(None, [0,0],None,None,[0,0],[0,0]), id='scatter-ratings'
     ),
-], className='card border-secondary mb-3')
+])
 
 line_device_time_div = html.Div([
-    html.Div([
     dcc.Graph(
-        figure = line_device_time(None, [0,0],None,None,[0,0],[0,0], 'mean'), id='line-device-time'
+        figure = line_device_time(None, [0,0],None,None,[0,0],[0,0]), id='line-device-time'
     ),
-    html.Div([
-        html.Label('Select below to change grouping of the data on the y-axis of graph above',htmlFor='line_device_radio'),
-        dcc.RadioItems(
-            options = {
-                'mean' : 'Average',
-                'count' : 'Count',
-                'sum' : 'Sum'
-            },
-            value = 'mean',
-            inline=True,
-            id='line_device_radio',
-            labelStyle={'display': 'inline-block','padding': '7px'}
-        )
-        ], style={"textAlign": "center",'margin-right':'3%','margin-left':'3%'}, className='row bg-light card mb-2')
-    ])
-], className='card border-secondary', style={'float':'right', 'width':'63%'})#f"{100*2/3-1}%"})
+], className='eight columns')
 
 # then organize into rows...
 row1 = html.Div([
     pie_devices_div,
     line_device_time_div
-], className='container row mb-3')
+], className='row')
 
 row2 = html.Div([
     scatter_ratings_div
-], className='container row')
+], className='row')
 
 # and then the main section all together
 main_div = html.Div([
     header_div,
     row1,
     row2
-], className = 'col-lg-9')
+], className = 'nine columns')
 
 # *************************************************************************************
 # ********************************** Utility Stuff ************************************
 # *************************************************************************************
 
 # add everything to the app layout
-app.layout = dbc.Container([
-    html.Div([
-        navBar,
-        main_div
-    ], className='row', style={'padding':'20px'}),
-    # add footer:
-    html.Footer([ 
-        html.P('This dashboard was created by Jonah Werbel for DS 4003 (taught by Dr. Natalie Kupperman) at UVA in Spring 2024.'),
-        html.P(['Data originally sourced from ',html.A('Kaggle', 
-            href='https://www.kaggle.com/datasets/mohammadhmozafary/\apples-product-placements-in-movies-and-tv-shows'),
-            '. See the GitHub repository with all work for this project ',html.A('here', 
-            href='https://github.com/JonahZW/apple-product-placement/'),'.'])
-    ], className='row text-light bg-dark p-4', style={'text-align':'center'})
-], className='dbc', fluid=True)
-
-app.title='Apple Product Placements'
+app.layout = html.Div([
+    navBar,
+    main_div
+], className='row', style={'padding':'20px'})
 
 # run the app!
 if __name__ == '__main__':
